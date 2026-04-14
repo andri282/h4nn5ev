dba62897a7f3e76f857fb0364c3922bb98409b08
andri282 <andrie282@student.uns.ac.id>
2026-04-14 14:12:54 +0700

    .

--- data.js
+++ data.js
@@ -4,8 +4,8 @@ const database = [
   {
     id: 1,
     tipe: "motor",
-    nama: "Gesits G1 2022 Second",
-    harga: "Rp 15.500.000",
+    nama: "Uwinfly T3S Pro",
+    harga: "Rp 6.000.000",
     deskripsi: "Kondisi mulus 90%, pemakaian normal dalam kota. Baterai masih awet, range tembus 45-50km. Surat-surat lengkap, pajak hidup. Kelistrikan aman semua, tinggal gas.\n\nMinus: Lecet pemakaian di bodi samping kiri dikit.",
     images: ["https://images.unsplash.com/photo-1558981806-ec527fa84c39?auto=format&fit=crop&q=80&w=800", "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?auto=format&fit=crop&q=80&w=800"],
   },
@@ -18,11 +18,11 @@ const database = [
     images: ["https://images.unsplash.com/photo-1449426468159-d96dbf08f19f?auto=format&fit=crop&q=80&w=800"],
   },
   {
-    id: 3,
+    id: 1776150694252,
     tipe: "sparepart",
-    nama: "BMS Smart 20S 72V 100A",
-    harga: "Rp 850.000",
-    deskripsi: "BMS Smart dengan fitur Bluetooth. Bisa pantau tegangan per cell lewat HP. Cocok banget buat build baterai custom motor listrik.\n\nKelengkapan:\n- Unit BMS\n- Kabel Balance\n- Modul Bluetooth",
-    images: ["https://images.unsplash.com/photo-1581092160562-40aa08e78837?auto=format&fit=crop&q=80&w=800"],
+    nama: "BMS JIKONG 150/300 2A",
+    harga: "1350000",
+    deskripsi: "BMS JIKONG",
+    images: ["images/bms-jikong-150300-2a-1.jpg"],
   },
 ];
