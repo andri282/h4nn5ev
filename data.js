94f921a7c86468e2b7d51eaa4783e8d71a39d83c
andri282 <andrie282@student.uns.ac.id>
2026-04-14 14:55:26 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,18 +1,28 @@
+// File: data.js
+
 const database = [
+  {
+    id: 1776151587897,
+    tipe: "motor",
+    nama: "Alessa Ex 3000 Second",
+    harga: "8500000",
+    deskripsi: "Alessa EX3000\nSurat lengkap\nPajak ON\nBatre bawaan upgrade BMS JIKONG\nAtas Nama PT surat pelepasan ada\nharga 8.5jt",
+    images: ["images/alessa-ex-3000-second-1.jpg"],
+  },
+  {
+    id: 1776151505410,
+    tipe: "motor",
+    nama: "Uwinfly T3S PRO Second",
+    harga: "6000000",
+    deskripsi: "Uwinfly T3S Pro\nOff the road / belum bersurat\nKelengkapan bisa dengan batre 72v 20ah atau tanpa batre\nbisa dengan charger atau tanpa charger\nHarga 6jt termasuk batre dan charger",
+    images: ["images/t3pro.jpg"],
+  },
   {
     id: 1776150694252,
     tipe: "sparepart",
     nama: "BMS JIKONG 150/300 2A",
     harga: "1350000",
-    deskripsi: "BMS JIKONG original untuk custom battery.",
+    deskripsi: "BMS JIKONG",
     images: ["images/bms-jikong-150300-2a-1.jpg"],
-  },
-  {
-    id: 1776150694253,
-    tipe: "motor",
-    nama: "Uwinfly T3S PRO Second",
-    harga: "6000000",
-    deskripsi: "Uwinfly T3S Pro\nOff the road / belum bersurat\nKelengkapan bisa dengan batre 72v 20ah atau tanpa batre\nbisa dengan charger atau tanpa charger\nHarga 6jt termasuk batre dan charger",
-    images: ["images/uwinfly-t3s-pro-1.jpg"],
   },
 ];
